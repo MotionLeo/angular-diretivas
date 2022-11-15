77cc7c0cce7ef4fe69da0c6bc6bf4fa487309b05
Leo <leodaniel.1998@hotmail.com>
2022-11-14 23:42:06 -0300

    subindo repo diretivas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TabelaComponent } from './tabela/tabela.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent, pathMatch:'full'},
+  {path:'home', component: HomeComponent},
+  {path:'tabela', component: TabelaComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { TabelaComponent } from './tabela/tabela.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    TopbarComponent,
+    TabelaComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
